67c8f9164a3decee409f116007c59922e604c4bd
KimHartley <kimhartley1986@gmail.com>
2020-04-10 14:45:48 +1200

    type bar

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { Component } from "react"
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+
+import Alphabet from "./Alphabet"
+import Text from "./Text"
+
+class App extends Component {
+    state = { text: "" }
+
+    changeText(event) {
+        console.log(event.target.value)
+        this.setState({ text: event.target.value })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <input
+                    type="text"
+                    value={this.state.text}
+                    onChange={this.changeText.bind(this)}
+                    placeholder="Type here"
+                    style={{
+                        padding: ".6em",
+                        fontSize: "1.2em",
+                        margin: "0px auto",
+                        width: "80%"
+                    }}
+                />
+                <svg width="100%" height="600">
+                    <Text x="32" y="300" text={this.state.text} />
+                    {/* <Alphabet x={32} y={300} /> */}
+                </svg>
+            </div>
+        );
+    }
 }
 
 export default App
