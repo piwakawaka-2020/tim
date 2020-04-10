ba7c61e45ca1d8e3254b4c6e3315de41e682d8b4
tcodling <thomas.codling@outlook.com>
2020-04-10 13:18:51 +1200

    add click event for changing picture

--- client/components/RandomPicture.jsx
+++ client/components/RandomPicture.jsx
@@ -4,9 +4,17 @@ class RandomPicture extends React.Component {
   state = {
       backgroundImage: `url(images/randomPic${Math.floor(Math.random() * 8)}.jpg)`
   }
+
+  changePicture = () => {
+      console.log('changing pic')
+      this.setState({
+        backgroundImage: `url(images/randomPic${Math.floor(Math.random() * 8)}.jpg)`
+      })
+  }
+
   render() {
     return (
-        <div className='randomPic' style={this.state}></div>
+        <div onClick={this.changePicture} className='randomPic' style={this.state}></div>
     )
   }
 }
