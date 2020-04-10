37aae83028f01b36e68d92724a8648d6a728d32f
tcodling <thomas.codling@outlook.com>
2020-04-10 12:51:00 +1200

    add more colors

--- client/components/ColorSelector.jsx
+++ client/components/ColorSelector.jsx
@@ -15,6 +15,16 @@ const ColorSelector = (props) => {
     <ColorOption changeColor={props.changeColor} bgColor='pink' />
     <ColorOption changeColor={props.changeColor} bgColor='orange' />
     <ColorOption changeColor={props.changeColor} bgColor='brown' />
+    <ColorOption changeColor={props.changeColor} bgColor='lime' />
+    <ColorOption changeColor={props.changeColor} bgColor='coral' />
+    <ColorOption changeColor={props.changeColor} bgColor='teal' />
+    <ColorOption changeColor={props.changeColor} bgColor='maroon' />
+    <ColorOption changeColor={props.changeColor} bgColor='indigo' />
+    <ColorOption changeColor={props.changeColor} bgColor='navy' />
+    <ColorOption changeColor={props.changeColor} bgColor='gold' />
+    <ColorOption changeColor={props.changeColor} bgColor='crimson' />
+    <ColorOption changeColor={props.changeColor} bgColor='grey' />
+    <ColorOption changeColor={props.changeColor} bgColor='lightgrey' />
     <ColorOption changeColor={props.changeColor} bgColor='black' />
     <ColorOption changeColor={props.changeColor} bgColor='white' />
     </div>
